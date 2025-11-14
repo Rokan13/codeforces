26ed81c25ce38864541fe00fddc3d23e865cb73b
Shah Rokan <shahrokan3@gmail.com>
2025-11-15 00:01:44 +0600

    collatz problem

---
+++ cf.collatz.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+            int k,x;
+    cin>> k >> x ;
+    int sum=1;
+
+    for(int i=0;i<k;i++){
+        x=x*2;
+    }
+    cout<<x<<endl;
+    }
+}
