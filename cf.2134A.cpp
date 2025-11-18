dde9cbfd7d77a9fe9ae23e230acea3e3715e7dbc
Shah Rokan <shahrokan3@gmail.com>
2025-11-19 01:25:02 +0600

    2134A

---
+++ cf.2134A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >>t;
+    while(t--){
+        long long n,a,b;
+        cin >> n >>a >>b;
+
+        if((n - b) % 2 != 0)
+        {
+            cout << "NO\n";
+        }
+        else if(a <= b) {
+                cout << "YES\n";
+        }
+        else if((a - b) % 2 == 0) {
+                cout << "YES\n";
+        }
+        else {
+                cout << "NO\n";
+        }
+    }
+}
